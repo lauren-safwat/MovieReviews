7fb178710386cba8c38a7803e1dce459d2c2c6ec
Mariam Makram <36172189+mariammakram@users.noreply.github.com>
2021-05-29 19:46:10 +0200

    Trained the TF_IDF model using SVM classifier

--- MovieReviews.py
+++ MovieReviews.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -7,34 +8,50 @@ from sklearn.feature_extraction.text import CountVectorizer
 import os
 import re
 
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn.svm import LinearSVC
 
 reviews = []
-reviewLabel = []
+reviewLabel = np.zeros(2000)
 
-def divideData(tfidf_vec):
-    X_train, X_test, y_train, y_test = train_test_split(tfidf_vec, reviewLabel, test_size=0.2)
-    print(X_test)
+
+def trainTF_IDF():
+    kf = StratifiedKFold(n_splits=5, random_state=2, shuffle=True)
+    totalsvm = 0  # Accuracy measure on 2000 files
+    totalMatSvm = np.zeros((2, 2))  # Confusion matrix on 2000 files
+
+    for train_index, test_index in kf.split(reviews, reviewLabel):
+        X_train = [reviews[i] for i in train_index]
+        X_test = [reviews[i] for i in test_index]
+        y_train, y_test = reviewLabel[train_index], reviewLabel[test_index]
+
+        vectorizer = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True, stop_words='english')
+        train_corpus_tf_idf = vectorizer.fit_transform(X_train)
+        test_corpus_tf_idf = vectorizer.transform(X_test)
+        model = LinearSVC()
+        model.fit(train_corpus_tf_idf, y_train)
+        result = model.predict(test_corpus_tf_idf)
+
+        totalMatSvm = totalMatSvm + confusion_matrix(y_test, result)
+        totalsvm = totalsvm + sum(y_test == result)
+
+    return totalMatSvm, totalsvm
 
 
 def readReviews(path):
-    counter = 0
     for filename in os.listdir(path):
-        if(counter == 6):
-            break
-        counter=counter+1
         f = open(os.path.join(path, filename), "r")
         reviews.append(f.read())
 
+
 def preprocessing():
+    reviewLabel[0:1000] = 1
     for i in range(len(reviews)):
-        reviews[i] = re.sub('[\W]+', ' ', reviews[i])
-        tokens = word_tokenize(reviews[i])
-        for word in tokens:
-            if word in stopwords.words():
-                reviews[i] = reviews[i].replace(" "+word+" ", " ")
-    reviewLabel = [1] * 1000
-    reviewLabel.extend([0] * 1000)
+        reviews[i] = reviews[i].lower()
+        reviews[i] = re.sub('[^\sa-zA-Z]', '', reviews[i])
+        reviews[i] = " ".join([word for word in word_tokenize(reviews[i]) if word not in stopwords.words()])
+
 
 def generateTF_IDF():
     tfidf_vectorizer = TfidfVectorizer(use_idf=True)
@@ -42,21 +59,18 @@ def generateTF_IDF():
     df = pd.DataFrame(tfidf_vectors[0].T.todense(), index=tfidf_vectorizer.get_feature_names(), columns=["tfidf"])
     df.sort_values(by=["tfidf"], ascending=False)
     print(df)
-    return tfidf_vectors
-
+    return tfidf_vectors.toarray()
 
 
 def main():
-    path1 = "E:\\FCI\\fourth year\\Second term\\NLP\\Assignments\\Assignment 2\\txt_sentoken\\pos"
-    path2 = "E:\\FCI\\fourth year\\Second term\\NLP\\Assignments\\Assignment 2\\txt_sentoken\\neg"
+    path1 = "..\\txt_sentoken\\pos"
+    path2 = "..\\txt_sentoken\\neg"
     readReviews(path1)
     readReviews(path2)
     preprocessing()
-    print(reviews[0])
-    print()
-    tfidf_vectors = generateTF_IDF()
-    print()
-    divideData(tfidf_vectors)
+    totalMatSvm, totalsvm = trainTF_IDF()
+    print("totalMatSvm: ", totalMatSvm)
+    print("totalsvm: ", totalsvm/2000)
 
 main()
 
