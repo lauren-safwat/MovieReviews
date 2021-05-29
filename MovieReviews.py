b38839dec54fc5c46a30535e43df98c33262c75d
Lauren Safwat <36446574+lauren-safwat@users.noreply.github.com>
2021-05-29 22:02:39 +0200

    Pre-processing data function is modified Testing model with user input is added

--- MovieReviews.py
+++ MovieReviews.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import matplotlib as plt
 from nltk.corpus import stopwords
@@ -7,26 +6,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
 import os
 import re
-
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.svm import LinearSVC
 
+
 reviews = []
 reviewLabel = np.zeros(2000)
-
+vectorizer = TfidfVectorizer(use_idf=True)
 
 def trainTF_IDF():
-    kf = StratifiedKFold(n_splits=5, random_state=2, shuffle=True)
-    totalsvm = 0  # Accuracy measure on 2000 files
-    totalMatSvm = np.zeros((2, 2))  # Confusion matrix on 2000 files
+    kf = StratifiedKFold(n_splits=5, random_state=3, shuffle=True)
+    totalsvm = 0
+    totalMatSvm = np.zeros((2, 2))
 
     for train_index, test_index in kf.split(reviews, reviewLabel):
         X_train = [reviews[i] for i in train_index]
         X_test = [reviews[i] for i in test_index]
         y_train, y_test = reviewLabel[train_index], reviewLabel[test_index]
 
-        vectorizer = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True, stop_words='english')
         train_corpus_tf_idf = vectorizer.fit_transform(X_train)
         test_corpus_tf_idf = vectorizer.transform(X_test)
         model = LinearSVC()
@@ -36,7 +34,7 @@ def trainTF_IDF():
         totalMatSvm = totalMatSvm + confusion_matrix(y_test, result)
         totalsvm = totalsvm + sum(y_test == result)
 
-    return totalMatSvm, totalsvm
+    return model, totalMatSvm, totalsvm
 
 
 def readReviews(path):
@@ -47,32 +45,43 @@ def readReviews(path):
 
 def preprocessing():
     reviewLabel[0:1000] = 1
+    cachedStopWords = stopwords.words("english")
     for i in range(len(reviews)):
         reviews[i] = reviews[i].lower()
-        reviews[i] = re.sub('[^\sa-zA-Z]', '', reviews[i])
-        reviews[i] = " ".join([word for word in word_tokenize(reviews[i]) if word not in stopwords.words()])
+        reviews[i] = re.sub("[^\sa-zA-Z]", "", reviews[i])
+        reviews[i] = ' '.join([word for word in word_tokenize(reviews[i]) if word not in cachedStopWords])
 
 
-def generateTF_IDF():
-    tfidf_vectorizer = TfidfVectorizer(use_idf=True)
-    tfidf_vectors = tfidf_vectorizer.fit_transform(reviews)
-    df = pd.DataFrame(tfidf_vectors[0].T.todense(), index=tfidf_vectorizer.get_feature_names(), columns=["tfidf"])
-    df.sort_values(by=["tfidf"], ascending=False)
-    print(df)
-    return tfidf_vectors.toarray()
+def testModel(model):
+    while(True):
+        print("Enter your movie review: ")
+        lines = []
+        while True:
+            line = input()
+            if line:
+                lines.append(line)
+            else:
+                break
+        review = '\n'.join(lines)
+
+        tfidf = vectorizer.transform([review])
+        yPredicted = model.predict(tfidf)
+        if(yPredicted == 0):
+            print("Negative Review")
+        else:
+            print("Positive Review")
 
 
 def main():
-    path1 = "..\\txt_sentoken\\pos"
-    path2 = "..\\txt_sentoken\\neg"
+    path1 = "..\\review_polarity\\txt_sentoken\\pos"
+    path2 = "..\\review_polarity\\txt_sentoken\\neg"
     readReviews(path1)
     readReviews(path2)
     preprocessing()
-    totalMatSvm, totalsvm = trainTF_IDF()
+    model, totalMatSvm, totalsvm = trainTF_IDF()
     print("totalMatSvm: ", totalMatSvm)
-    print("totalsvm: ", totalsvm/2000)
+    print("totalsvm: ", (totalsvm/2000)*100, "%")
+    print()
+    testModel(model)
 
 main()
-
-#clf = linear_model.SGDClassifier()
-#clf.fit(tfidf_vec, reviewLabel)
