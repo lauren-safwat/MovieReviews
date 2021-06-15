dd78796f93ef3420820a77a42ce357f8857ae1ca
Mariam Makram <36172189+mariammakram@users.noreply.github.com>
2021-06-15 03:02:48 +0200

    Created word embedding and sentence embedding

--- MovieReviews.py
+++ MovieReviews.py
@@ -2,11 +2,15 @@ import numpy as np
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from yellowbrick.text import TSNEVisualizer
 import os
 import re
+import gensim
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # ============================================================================
 
@@ -28,11 +32,57 @@ def readReviews(path):
 
 def preprocessing():
     reviewsLabel[0:1000] = 1
-    cachedStopWords = stopwords.words("english")
+    stopWords = stopwords.words("english")
+    moreChars = ['\'s', '\'re', '(', '?', ')', '.', ',', ':', '-', '/', '``', '\'ll']
+    stopWords.remove('not')
+    stopWords.extend(moreChars)
+
     for i in range(len(reviews)):
-        reviews[i] = reviews[i].lower()
-        reviews[i] = re.sub("[^\sa-zA-Z]", "", reviews[i])
-        reviews[i] = ' '.join([word for word in word_tokenize(reviews[i]) if word not in cachedStopWords])
+        gensim.utils.simple_preprocess(reviews[i])
+        reviews[i] = re.sub("[0-9]", "", reviews[i])
+        reviews[i] = re.sub("[/]", ' ', reviews[i])
+        reviews[i] = [word for word in word_tokenize(reviews[i]) if word not in stopWords and (len(word) > 2)]
+
+
+# ============================================================================
+
+
+def createWordEmbedding():
+    model = gensim.models.Word2Vec(reviews, vector_size=150, window=5, min_count=1, workers=8)
+    model.train(reviews, total_examples=len(reviews), epochs=10)
+    return model
+
+
+# ============================================================================
+
+
+def sentenceEmbedding(model):
+    reviewFeatureVec = []
+    for review in reviews:
+        review = [word for word in review if word in model.wv.index_to_key]    # remove out-of-vocabulary words
+        if len(review) >= 1:
+            reviewFeatureVec.append(np.mean(model.wv[review], axis=0))
+        else:
+            reviewFeatureVec.append([])
+
+    return reviewFeatureVec
+
+# ============================================================================
+
+
+def svc_param_selection(X, y, nfolds):
+    Cs = [0.001, 0.01, 0.1, 1, 10]
+    gammas = [0.001, 0.01, 0.1, 1]
+    param_grid = {'C': Cs, 'gamma' : gammas}
+    grid_search = GridSearchCV(SVC(kernel='rbf'), param_grid, cv=nfolds)
+    grid_search.fit(X, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+
+# ============================================================================
+
+
 
 
 # ============================================================================
@@ -61,7 +111,7 @@ def trainTF_IDF():
 # ============================================================================
 
 
-def testModel(model):
+def testModel(model, is_tfidf):
     print("Enter your movie review: ")
     lines = []
     while True:
@@ -72,8 +122,13 @@ def testModel(model):
             break
     review = '\n'.join(lines)
 
-    tfidf = tfidf_vectorizer.transform([review])
-    yPredicted = model.predict(tfidf)
+    if is_tfidf:
+        review = tfidf_vectorizer.transform([review])
+
+    # else:
+    #     review =
+
+    yPredicted = model.predict(review)
     if (yPredicted == 0):
         print("Negative Review")
     else:
@@ -102,10 +157,15 @@ def main():
     readReviews(path1)
     readReviews(path2)
     preprocessing()
-    model, nCorrectPred = trainTF_IDF()
-    print("Accuracy: ", (nCorrectPred / 2000) * 100, "%")
-    print()
-    testModel(model)
-    plotData()
+    # model, nCorrectPred = trainTF_IDF()
+    # print("Accuracy: ", (nCorrectPred / 2000) * 100, "%")
+    # print()
+    # testModel(model, true)
+    #plotData()
+
+    word2vecModel = createWordEmbedding()
+    reviewFeatureVec = sentenceEmbedding(word2vecModel)
+
+
 
 main()
